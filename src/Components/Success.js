f4b4477c9414ef7a9524ff2953e773d05bd55c54
Skypo <32127408+OfficialSkypo@users.noreply.github.com>
2021-07-06 20:01:23 +0200

    Update Success.js

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#00e676"}}>Success! Your ban appeal has been submitted to OpenCAD Management!</h1>
-                <h4>Please allow some time for them to review your appeal. Abusing this system will result in a perma-ban.</h4>
+                <h1 style={{textAlign: "center", color: "#00e676"}}>Sent! Your ban appeal has been submitted to Team Snaily!</h1>
+                <h4>Please allow some time for them to review your appeal. Abusing of this system will result in a permanent ban.</h4>
             </Grid>
         );
     }
