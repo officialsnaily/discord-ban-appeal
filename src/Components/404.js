dc84b9ec94bdddd7c13b31623be3b5b54f1cbd8c
Skypo <32127408+OfficialSkypo@users.noreply.github.com>
2021-07-07 03:00:38 +0200

    Update 404.js

--- src/Components/404.js
+++ src/Components/404.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import PageNotFound from '../Images/readingtunic.jpg'
+import PageNotFound from '../Images/readingtunic.png'
 import {NavLink} from "react-router-dom";
 class PageNotFoundError extends Component
 {
@@ -8,7 +8,7 @@ class PageNotFoundError extends Component
         return (
             <div>
                 <img alt="404 tunic" src={PageNotFound}/>
-                <h1>404 Snaily Not Found!</h1>
+                <h1 style={{textAlign: "center", color: "#000000"}}>404 Snaily Not Found!</h1>
                 <h2>Go on! Slowly crawl back to where you came from..</h2>
                 <NavLink to="/">
                     Go Home
