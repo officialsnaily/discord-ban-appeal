5071740d8283b64ce201cae10665df1c759f8f6a
Skypo <32127408+OfficialSkypo@users.noreply.github.com>
2022-10-06 03:16:56 +0200

    Update Success.js

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -5,8 +5,8 @@ class Success extends Component {
     render() {
         return (
             <Grid item>
-                <h1 style={{textAlign: "center", color: "#000000"}}>Sent! Your ban appeal has been submitted to Team Snaily!</h1>
-                <h4>Please allow some time for them to review your appeal. Abuse of this system will result in punishment.</h4>
+                <h1 style={{textAlign: "center", color: "#000000"}}>Done! Your ban appeal has been submitted to Team Snaily!</h1>
+                <h4>It may take some time to review your ban appeal so please be patient. Abuse of this system will result in punishment.</h4>
             </Grid>
         );
     }
