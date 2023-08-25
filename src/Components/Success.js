624b7136abe74c7f9b7a0b30096ee2ee4b0ae05a
Skypo <32127408+OfficialSkypo@users.noreply.github.com>
2023-08-25 12:09:01 +0200

    Update Success.js
    Formatting update.

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -6,7 +6,7 @@ class Success extends Component {
         return (
             <Grid item>
                 <h1 style={{textAlign: "center", color: "#000000"}}>Done! Your ban appeal has been submitted to Team Snaily!</h1>
-                <h4>It may take some time to review your ban appeal so please be patient. Abuse of this system will result in punishment.</h4>
+                <h4>It may take some time (but no longer than 1 month) to review your ban appeal so please be patient. Abuse of this system will result in punishment. If you are still unable to join our Discord Community 1 month after your appeal, your appeal has been denied.</h4>
             </Grid>
         );
     }
